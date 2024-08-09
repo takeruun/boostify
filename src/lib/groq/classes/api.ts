1f109df8cc6938e7dd304c6e0f8b12fbffa1de76
takeru.hashimoto <hstake25@gmail.com>
2024-08-10 00:13:03 +0900

    修正

--- src/lib/groq/classes/api.ts
+++ src/lib/groq/classes/api.ts
@@ -1,4 +1,4 @@
-import Groq from "groq-sdk";
+import Groq, { APIError } from "groq-sdk";
 import { SYSTEM_PROMPT } from "../constants";
 import { GroqApiConstructor, GroqApiInterface } from "../interfaces/classes/api";
 
@@ -11,27 +11,47 @@ export class GroqApi {
   private model: string = 'llama3-8b-8192';
 
   constructor(API_KEY: string) {
+    if (!API_KEY) {
+      throw new Error('API_KEY is required');
+    }
+
     this.client = new Groq({
       apiKey: API_KEY,
     });
   }
 
   async postEvaluation(content: string): Promise<boolean> {
-    const res = await this.client.chat.completions.create({
-      messages: [
-        {
-          role: 'system', content: SYSTEM_PROMPT
-        },
-        {
-          role: 'user', content
+    try {
+      const res = await this.client.chat.completions.create({
+        messages: [
+          {
+            role: 'system', content: SYSTEM_PROMPT
+          },
+          {
+            role: 'user', content
+          }
+        ],
+        model: this.model,
+      });
+      const resContent = res.choices[0].message.content;
+      if (!resContent) {
+        throw new Error('Failed to evaluate post content');
+      }
+
+      return resContent.includes('YES');
+    } catch (error) {
+      if (error instanceof APIError) {
+        console.log(error);
+        switch (error.status) {
+          case 403:
+            throw new Error('Invalid API key');
         }
-      ],
-      model: this.model,
-    });
-    const resContent = res.choices[0].message.content;
-    if (!resContent) {
-      return false;
+      }
+      else {
+        throw error;
+      }
     }
-    return resContent.includes('YES');
+
+    return false;
   }
 }
