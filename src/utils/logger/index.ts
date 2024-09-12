aa758a94689e3821bf7121c280f94756ca83f02f
takeru.hashimoto <hstake25@gmail.com>
2024-09-13 02:26:16 +0900

    log

---
+++ src/utils/logger/index.ts
@@ -0,0 +1,47 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { createLogger, format, Logger, transports } from 'winston';
+
+export class BoostfiyLogger {
+  private logger: Logger = createLogger({
+    level: 'info',
+    format: format.combine(
+      format.timestamp({
+        format: 'YYYY-MM-DD HH:mm:ss',
+      }),
+      format.errors({ stack: true }),
+      format.splat(),
+      format.json(),
+    ),
+    defaultMeta: { service: 'boostfiy' },
+    transports: [
+      new transports.File({ filename: 'log/error.log', level: 'error' }),
+      new transports.File({ filename: 'log/development.log' }),
+    ],
+  });
+
+  constructor() {
+    if (process.env.NODE_ENV !== 'production') {
+      this.logger.add(
+        new transports.Console({
+          format: format.combine(format.colorize(), format.simple()),
+        }),
+      );
+    }
+  }
+
+  public info(message: string, meta?: any) {
+    this.logger.info(message, meta);
+  }
+
+  public error(message: string, meta?: any) {
+    this.logger.error(message, meta);
+  }
+
+  public warn(message: string, meta?: any) {
+    this.logger.warn(message, meta);
+  }
+
+  public debug(message: string, meta?: any) {
+    this.logger.debug(message, meta);
+  }
+}
