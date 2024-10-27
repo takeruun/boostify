5a79b03b23209cac6ee5d198a5f42dd2c28131ae
takeru.hashimoto <hstake25@gmail.com>
2024-10-27 22:27:32 +0900

    rm

--- src/lib/twitter/schemas/tweet.ts
+++
@@ -1,6 +0,0 @@
-import { FavoriteTweetResponse } from 'src/lib/twitter/types/tweet';
-import { z } from 'zod';
-
-export const FavoriteTweetResponseSchema = z.object({
-  data: z.object({ favorite_tweet: z.literal('Done') }),
-}) satisfies z.ZodType<FavoriteTweetResponse>;
